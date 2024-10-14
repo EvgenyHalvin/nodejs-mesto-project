8a4cb26d68978da896141d6d2081389a9d8bee38
eugene.zhdanov <evgeny1halvin@yandex.ru>
2024-10-14 17:14:49 +0300

    chore(user): expand user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,23 @@
-import { model, Schema } from "mongoose";
+import { Model, model, Schema, Document } from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
 
 type TUser = {
   name: string;
   about: string;
   avatar: string;
+  email: string;
+  password: string;
 };
 
-const userSchema = new Schema<TUser>(
+type TUserModel = Model<TUser> & {
+  findUserByCredentials: (
+    email: string,
+    password: string
+  ) => Promise<Document<unknown, unknown, TUser>>;
+};
+
+const userSchema = new Schema<TUser, TUserModel>(
   {
     name: {
       type: String,
@@ -23,9 +34,47 @@ const userSchema = new Schema<TUser>(
     avatar: {
       type: String,
       required: true,
+      validate: {
+        validator: (url) => validator.isURL(url),
+        message: "Некорректная ссылка на аватар",
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email) => validator.isEmail(email),
+        message: "Некорректный email",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
     },
   },
   { versionKey: false }
+);
+
+userSchema.static(
+  "findUserByCredentials",
+  function findUserByCredentials(email: string, password: string) {
+    return this.findOne({ email }).then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Неправильные почта или пароль"));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Неправильные почта или пароль"));
+        }
+
+        return user;
+      });
+    });
+  }
 );
 
 export default model("user", userSchema);
